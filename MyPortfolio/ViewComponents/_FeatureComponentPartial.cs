2619708d848e4937c6e9ccae2d1916ea1ea42ccd
ozgeozkur <ozge.zz53@example.com>
2024-11-07 19:26:05 +0300

    .

---
+++ MyPortfolio/ViewComponents/_FeatureComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _FeatureComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke() {  return View(); }
+    }
+}
