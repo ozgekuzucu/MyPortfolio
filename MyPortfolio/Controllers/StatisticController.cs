01c96227127edf46b0c466a9deec89657b099408
ozgeozkur <ozge.zz53@example.com>
2025-04-13 19:53:51 +0300

    .

--- MyPortfolio/Controllers/StatisticController.cs
+++ MyPortfolio/Controllers/StatisticController.cs
@@ -12,6 +12,11 @@ namespace MyPortfolio.Controllers
 			ViewBag.v2 = context.Messages.Count();
 			ViewBag.v3 = context.Messages.Where(x => x.IsRead == false).Count();
 			ViewBag.v4 = context.Messages.Where(x => x.IsRead == true).Count();
+			ViewBag.v5 = context.Experiences.Count();
+			ViewBag.v6 = context.Skills.Count();
+			ViewBag.v7 =context.Testimonials.Count();
+			ViewBag.v8 =context.ToDoLists.Where(x=>x.Status==true).Count();
+			ViewBag.v9 =context.ToDoLists.Where(x=>x.Status==false).Count();
 			return View();
 		}
 	}
